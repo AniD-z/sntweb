4b8e2d2d26d28fd8024930f5ba49a732db4b7d2e
AniD-z <137194673+AniD-z@users.noreply.github.com>
2025-06-10 10:51:24 +0530

    remove

--- lib/components/ui/container.tsx
+++
@@ -1,9 +0,0 @@
-type ContainerProps = {
-  children: React.ReactNode;
-};
-
-const Container: React.FC<ContainerProps> = ({ children }) => {
-  return <div className="container 2xl mx-auto w-full">{children}</div>;
-};
-
-export default Container;
