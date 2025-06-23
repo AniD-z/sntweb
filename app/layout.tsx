6f010cdb2e61b620cb7c73939365a28d3d3adc4b
AniD-z <137194673+AniD-z@users.noreply.github.com>
2025-06-24 02:00:39 +0530

    DEL POD

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import PodcastContextProvider from "@/context/PodcastContext";
+
 import ArticleContextProvider from "@/context/ArticleContext";
 
 export const metadata: Metadata = {
